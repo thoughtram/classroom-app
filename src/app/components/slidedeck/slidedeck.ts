d088ea2f1a8e1da386570721bf444ba9722311ab
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2015-10-24 22:27:26 +0200

    fix: bump to /login if iframe fails to load

--- src/app/components/slidedeck/slidedeck.ts
+++ src/app/components/slidedeck/slidedeck.ts
@@ -1,4 +1,5 @@
 import { LocationPatchupService } from './location_patchup_service';
+import { ApiService } from '../../common/api_service';
 
   export let ClassroomSlidedeckModule = angular.module('classroom.slidedeck', [])
   .service('locationPatchupService', LocationPatchupService)
@@ -12,7 +13,16 @@ import { LocationPatchupService } from './location_patchup_service';
             width="100%" height="100%">
           </iframe>`,
         controllerAs: 'ctrl',
-        controller: function (config: any, $stateParams: any, $sce: ng.ISCEService) {
+        controller: function (config: any, $stateParams: any, $sce: ng.ISCEService, apiService: ApiService) {
+
+          // That's a bit of a hack. We can't figure out if the iframe failed
+          // to load when the user isn't logged in. The same origin policy prevents
+          // us from getting such insights.
+          // Instead we figure out if the user is logged in by trying to use
+          // the API. If it fails, the global error handler will do the redirect
+          // for us.
+          apiService.getUser();
+
           this.className = $stateParams.className;
           var pageFragment = $stateParams.page ? '#/' + $stateParams.page : '';
           this.iframeSrc = $sce.trustAsResourceUrl(config.API_ENDPOINT + '/secure/'+ $stateParams.className + '/' + $stateParams.deckName + '/index.html' + pageFragment);
